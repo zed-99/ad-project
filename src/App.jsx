6b8781440ed1fca8f618f888e6c7d3c7a32eb865
doubao9 <3223556260@qq.com>
2023-03-14 14:57:01 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import './style.scss';
+import './style.scss'
+class App extends Component {
+    render() {
+        return (
+            <div className="box">
+                Hello , World
+                <span>i,am jack</span>
+            </div>
+        );
+    }
+}
+
+export default App;
